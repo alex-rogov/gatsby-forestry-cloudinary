f3442c513707e32a5439899a9908b5552a081124
alex-rogov <omorpen@gmail.com>
2022-01-24 12:38:33 +0200

    s

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `content`,
-        path: `${__dirname}/content`,
+        path: `${__dirname}/src/content`,
       },
     },
     {
